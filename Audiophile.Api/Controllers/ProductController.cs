6e58a8b037517da28911f78a1a66f2ab09e4c0f2
Chris-LeBlanc <146110323+Chris-LeBlanc@users.noreply.github.com>
2025-12-26 21:51:27 -0400

    Implemented GetAll functionality for Products controller

---
+++ Audiophile.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+using System.Runtime.InteropServices.Marshalling;
+using Audiophile.Models;
+using Audiophile.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Audiophile.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<List<ProductListDto>> Index()
+        {
+            return await _productService.GetProductsAsync();
+        }
+    }
+}
